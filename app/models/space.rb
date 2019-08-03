690bd572ae8dea1bf44336e07dd5bcca4e5d6a4c
Walter Cegarra <walter.cegarra@icloud.com>
2019-08-03 14:49:51 +0100

    Added relations between models

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,3 +1,4 @@
 class Space < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_134229) do
+ActiveRecord::Schema.define(version: 2019_08_03_134737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "space_id"
+    t.date "date_start"
+    t.date "date_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["space_id"], name: "index_bookings_on_space_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "spaces", force: :cascade do |t|
     t.string "name"
@@ -42,5 +53,7 @@ ActiveRecord::Schema.define(version: 2019_08_03_134229) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "spaces"
+  add_foreign_key "bookings", "users"
   add_foreign_key "spaces", "users"
 end
