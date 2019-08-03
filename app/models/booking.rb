31c6eaacf19664f6a7644d5a159b5951881f00c4
Walter Cegarra <walter.cegarra@icloud.com>
2019-08-03 15:20:34 +0100

    updated

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :space
-  validates :user, :space, :date_start, :date_end, presence: true
+  # validates :user, :space, :date_start, :date_end, presence: true
 end
