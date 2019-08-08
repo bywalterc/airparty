a8eb024bb1e662ae46713ec79f49c18bd04cb196
Walter Cegarra <walter.cegarra@icloud.com>
2019-08-08 20:19:51 +0100

    Implement Booking controller and form in Show view

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  before_action :set_space, except: [:destroy]
+
+  def new
+    @booking = Booking.new
+    raise
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.space = @space
+    @booking.user = current_user
+    if @booking.save
+      redirect_to space_path(@space)
+    else
+      render 'spaces/show'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :date_start, :date_end)
+  end
+
+  def set_space
+    @space = Space.find(params[:space_id])
+  end
+end
--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -7,6 +7,7 @@ class SpacesController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
