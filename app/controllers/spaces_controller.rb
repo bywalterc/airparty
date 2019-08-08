04b51df2a946063c4264d54f1657d0ef126eab0c
Spencer Terry <spencerterry1@gmail.com>
2019-08-08 21:32:49 +0100

    authenticate user

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:home, :index]
   before_action :set_space, only: [:show, :edit, :update, :destroy]
 
   def index
