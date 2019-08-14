f4df9680db141dfb80e3310601ef8b8166ea1cd2
Spencer Terry <spencerterry1@gmail.com>
2019-08-14 22:36:30 +0100

    added calendar to bookings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,4 +6,11 @@ import { initMapbox } from "../plugins/init_mapbox";
 
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
+import "../plugins/flatpickr"
+
+
+flatpickr(".datepicker", {
+  altInput: true
+});
+
 initMapbox();
